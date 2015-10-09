99197da2fa030bdcebe0ec2d6f9da41c563757ca
Doug Walter <doug@kosmojo.com>
2015-10-09 10:11:06 -0600

    first commit

---
+++ shape_test.spec.js
@@ -0,0 +1,54 @@
+var shape = require('./shapes');
+
+describe('Shape', function() {
+
+  var square = new shape.Shape2d("square", "orange");
+  var circle = new shape.Circle("circle", "blue", 3);
+  var cube = new shape.Cube("cube", "purple", 3);
+
+
+  it('The square should be orange', function() {
+    expect(square.getColor()).toEqual('orange');
+  });
+
+  xit('The circle should be blue', function() {
+    expect(circle.getColor()).toEqual('blue');
+  });
+
+  xit('What is the circles radius?', function(){
+    expect(circle.getRadius()).toEqual(3);
+  });
+
+  xit('What is the circles diameter? ', function(){
+    expect(circle.getDiameter()).toEqual(6);
+  });
+
+  xit('What is the circles area? ', function(){
+    expect(circle.getArea()).toEqual(18.84955592153876);
+  });
+
+  xit('circle is a 2D Shape', function() {
+    expect(circle instanceof shape.Shape2d).toBe(true);
+  });
+
+  xit('The cube should be purple', function() {
+    expect(cube.getColor()).toEqual('purple');
+  });
+
+  xit('What is the cubes volume?', function() {
+    expect(cube.getVolume()).toEqual(27);
+  });
+
+  xit('the cube should be a 3D Shape', function() {
+    expect(cube instanceof shape.Shape3d).toBe(true);
+  });
+
+  xit('the cube should be a Shape', function() {
+    expect(cube instanceof shape.Shape).toBe(true);
+  });
+
+  xit('the circle should not have volume', function() {
+    expect(circle.getVolume).toBe(undefined);
+  });
+
+});
---
+++ shapes.js
@@ -0,0 +1,27 @@
+exports.Shape = function() {
+
+}
+
+exports.Shape2d = function(){
+	
+}
+//Voodoo Magic to make Shape2d inherit from Shape
+
+exports.Circle = function(name, color, radius){
+
+}
+//Voodoo Magic to make Circle inherit from Shape2d
+
+exports.Shape3d = function(){
+	
+}
+//Voodoo Magic to make Shape3d inherit from Shape
+
+exports.Cube = function(name, color, radius){
+
+}
+//Voodoo Magic to make Cube inherit from Shape3d
+
+
+
+
